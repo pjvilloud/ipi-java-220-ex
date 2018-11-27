1399e0df391888d7bd1442086f4f06917201beb7
mathieupruvot <mathieu.pruvot@gmail.com>
2018-11-27 11:02:00 +0100

    ajout class DateEmbaucheException

---
+++ src/main/java/com/ipiecoles/java/java220/DateEmbaucheException.java
@@ -0,0 +1,13 @@
+package com.ipiecoles.java.java220;
+
+public class DateEmbaucheException extends Exception {
+
+    public DateEmbaucheException(){
+        super();
+    }
+
+    public DateEmbaucheException(String message){
+        super(message);
+    }
+
+}
--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,7 +1,72 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 /**
  * Created by pjvilloud on 21/09/17.
  */
 public class Employe {
+
+    private String nom;
+    private String prenom;
+    private String matricule;
+    private LocalDate dateEmbauche;
+    private Double salaire;
+
+    public Employe(){
+
+    }
+
+    public Employe(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+        this.dateEmbauche = dateEmbauche;
+        this.salaire = salaire;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public void setDateEmbauche(LocalDate dateEmbauche) {
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public void setSalaire(Double salaire) {
+        this.salaire = salaire;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public LocalDate getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public Double getSalaire() {
+        return salaire;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    private Integer getNombreAnneeAnciennete(){
+        return LocalDate.now().getYear() - this.dateEmbauche.getYear();
+    }
+
 }
