e5e64c28f73e4e06c9838dda7ffe62c5809d2f7e
mathieupruvot <mathieu.pruvot@gmail.com>
2018-11-27 15:31:38 +0100

    ajout class Commercial

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -11,11 +11,13 @@ public class Commercial extends Employe {
 
     public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire) {
         super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel=0d;
     }
 
     @Override
     public Double getPrimeAnnuelle() {
-        return null;
+        if(this.caAnnuel==null) return 500d;
+        return (this.caAnnuel*0.05<500 ? 500 : Math.rint(this.caAnnuel*0.05));
     }
 
     public Double getCaAnnuel() {
