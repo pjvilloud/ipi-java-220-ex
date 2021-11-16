57ffd337a47004de945d941132a16294dbba6e00
Roja <roja.ghaffari@etudiant.univ-reims.fr>
2021-11-16 11:58:10 +0100

    Exo 101,102,103

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,9 +1,70 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 /**
  * Started Project by Roja in 16/11/2021.
  */
 public class Employe {
+
+    private String nom;
+    private String prenom;
+    private String matricule;
+    private LocalDate dateEmbauche;
+    private Double salaire;
+
+    public Employe(){}
+
+    public Employe(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+        this.dateEmbauche = dateEmbauche;
+        this.salaire = salaire;
+    }
+
+    public final Integer getNombreAnneeAnciennete(){
+        return LocalDate.now().getYear() - dateEmbauche.getYear();
+    }
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public LocalDate getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public void setDateEmbauche(LocalDate dateEmbauche) {
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public Double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(Double salaire) {
+        this.salaire = salaire;
+    }
 
 }
 
---
+++ src/main/java/com/ipiecoles/java/java220/Main.java
@@ -0,0 +1,9 @@
+package com.ipiecoles.java.java220;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Hello to my Java world");
+        System.out.println(Entreprise.primeAnnuelleBase());
+        Employe employe = new Employe();
+    }
+}
