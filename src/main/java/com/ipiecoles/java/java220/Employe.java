ec8f7e89f19d6833f08c2f5f70178846a8dd96d8
Roja <roja.ghaffari@etudiant.univ-reims.fr>
2021-11-16 15:31:59 +0100

    Exo 104-110

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -2,6 +2,8 @@ package com.ipiecoles.java.java220;
 
 import org.joda.time.LocalDate;
 
+import java.util.Objects;
+
 /**
  * Started Project by Roja in 16/11/2021.
  */
@@ -24,7 +26,7 @@ public class Employe {
     }
 
     public final Integer getNombreAnneeAnciennete(){
-        return LocalDate.now().getYear() - dateEmbauche.getYear();
+        return LocalDate.now().getYear() - this.dateEmbauche.getYear();
     }
     public String getNom() {
         return nom;
@@ -54,7 +56,10 @@ public class Employe {
         return dateEmbauche;
     }
 
-    public void setDateEmbauche(LocalDate dateEmbauche) {
+    public void setDateEmbauche(LocalDate dateEmbauche) throws Exception{
+        if (dateEmbauche != null && dateEmbauche.isAfter(LocalDate.now())){
+            throw new Exception("La date d'embauche ne peut être postérieure à la date courante");
+        }
         this.dateEmbauche = dateEmbauche;
     }
 
@@ -64,6 +69,41 @@ public class Employe {
 
     public void setSalaire(Double salaire) {
         this.salaire = salaire;
+    }
+    public Integer getNbConges(){
+        return Entreprise.NB_CONGES_BASE;
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", matricule='" + matricule + '\'' +
+                ", dateEmbauche=" + dateEmbauche +
+                ", salaire=" + salaire +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employe employe = (Employe) o;
+        return Objects.equals(nom, employe.nom) &&
+                Objects.equals(prenom, employe.prenom) &&
+                Objects.equals(matricule, employe.matricule) &&
+                Objects.equals(dateEmbauche, employe.dateEmbauche) &&
+                Objects.equals(salaire, employe.salaire);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, prenom, matricule, dateEmbauche, salaire);
+    }
+    public void augmenterSalaire(Double pourcentage){
+        this.salaire = this.getSalaire() * (1 + pourcentage);
+    }
+    public  Double getPrimeAnnuelle(){
+        return Entreprise.primeAnnuelleBase();
     }
 
 }
--- src/main/java/com/ipiecoles/java/java220/Main.java
+++ src/main/java/com/ipiecoles/java/java220/Main.java
@@ -1,9 +1,18 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello to my Java world");
         System.out.println(Entreprise.primeAnnuelleBase());
         Employe employe = new Employe();
+        Employe em1 = new Employe("justin", "carette", "J4587", new LocalDate(2019, 12, 15) ,10000.0);
+        System.out.println(em1.getNom()+" have "+em1.getNombreAnneeAnciennete()+" ans experience in this company");
+        System.out.println("employer info :"+em1.toString());
+        em1.equals("justin");
+        System.out.println(em1.getNom().equals("justin"));
+        System.out.println(em1.hashCode());
+
     }
 }
