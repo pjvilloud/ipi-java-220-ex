3f6307ae5c12091a8f95649c0fbf2815ae9feaeb
Manon <manon.orsini5@gmail.com>
2021-12-02 19:29:30 +0100

    Employe ajout tempsPartiel et Sexe (Constructeur + Get/Set)

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,7 +1,33 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 /**
  * Created by pjvilloud on 21/09/17.
  */
 public class Employe {
+
+    private Boolean tempsPartiel;
+    private String sexe;
+
+    public Employe(Boolean tempsPartiel, String sexe) {
+        this.tempsPartiel = tempsPartiel;
+        this.sexe = sexe;
+    }
+
+    public Boolean getTempsPartiel() {
+        return tempsPartiel;
+    }
+
+    public void setTempsPartiel(Boolean tempsPartiel) {
+        this.tempsPartiel = tempsPartiel;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
 }
